36450ad9e6c28eacc6c8d09d481dd3b57a0b5939
Jared Baur <jaredbaur@fastmail.com>
2022-11-29 14:03:28 -0800

    Update deps

--- default.nix
+++ default.nix
@@ -3,6 +3,6 @@ buildGoModule {
   pname = "pd-notify";
   version = "0.1.0";
   src = ./.;
-  vendorSha256 = "sha256-QJenPUIhfIXDHfCHoxX6CqOn+MGZrQPL97tvYP6iNno=";
+  vendorSha256 = "sha256-kEeS+X45Mmo7yNrA0MpChhYguk+c3ayVSNO7i6/daJY=";
   inherit CGO_ENABLED;
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/jmbaur/pd-notify
 
 go 1.18
 
-require github.com/PagerDuty/go-pagerduty v1.5.1
+require github.com/PagerDuty/go-pagerduty v1.6.0
 
 require github.com/google/go-querystring v1.1.0 // indirect
