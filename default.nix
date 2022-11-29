2d271ef441191f76cda63c5dab52400175983f95
Jared Baur <jaredbaur@fastmail.com>
2022-11-29 14:13:42 -0800

    Version bump

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { buildGoModule, writeShellScriptBin, CGO_ENABLED ? 0 }:
 buildGoModule {
   pname = "pd-notify";
-  version = "0.1.0";
+  version = "0.1.1";
   src = ./.;
   vendorSha256 = "sha256-kEeS+X45Mmo7yNrA0MpChhYguk+c3ayVSNO7i6/daJY=";
   inherit CGO_ENABLED;
