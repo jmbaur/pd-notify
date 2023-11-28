997138a9fe77d5968e58f3a3433885a522023cbd
Jared Baur <jaredbaur@fastmail.com>
2023-11-27 22:56:57 -0800

    Use vendorHash

--- default.nix
+++ default.nix
@@ -3,5 +3,5 @@ buildGoModule {
   pname = "pd-notify";
   version = "0.1.1";
   src = ./.;
-  vendorSha256 = "sha256-kEeS+X45Mmo7yNrA0MpChhYguk+c3ayVSNO7i6/daJY=";
+  vendorHash = "sha256-kEeS+X45Mmo7yNrA0MpChhYguk+c3ayVSNO7i6/daJY=";
 }
--- flake.nix
+++ flake.nix
@@ -1,28 +1,24 @@
 {
   description = "pd-notify";
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-  outputs = inputs: with inputs;
-    let
-      forAllSystems = f: nixpkgs.lib.genAttrs
-        [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ]
-        (system: f {
-          inherit system;
-          pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
-        });
-    in
-    {
-      overlays.default = final: prev: { pd-notify = prev.callPackage ./. { }; };
-      packages = forAllSystems ({ pkgs, ... }: {
-        default = pkgs.pd-notify;
-      });
-      apps = forAllSystems ({ pkgs, ... }: {
+  outputs = inputs: with inputs; {
+    overlays.default = final: prev: { pd-notify = prev.callPackage ./. { }; };
+    legacyPackages = nixpkgs.lib.genAttrs
+      [ "aarch64-linux" "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ]
+      (system: import nixpkgs { inherit system; overlays = [ self.overlays.default ]; });
+    apps = nixpkgs.lib.mapAttrs
+      (_: pkgs: {
         default = { type = "app"; program = "${pkgs.pd-notify}/bin/pd-notify"; };
-      });
-      devShells = forAllSystems ({ pkgs, ... }: {
+      })
+      self.legacyPackages;
+    devShells = nixpkgs.lib.mapAttrs
+      (_: pkgs: {
         default = pkgs.mkShell {
           inherit (pkgs.pd-notify) nativeBuildInputs;
           buildInputs = with pkgs; [ revive ];
         };
-      });
-    };
+      })
+      self.legacyPackages;
+  };
 }
+
